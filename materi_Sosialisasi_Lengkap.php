c8c70f3f8ec1badadd5ce532afe5b9d03b43c47d
bintangdemarta <bintangdemarta@gmail.com>
2024-02-19 03:04:58 +0700

    menambahkan alert

--- materi_Sosialisasi_Lengkap.php
+++ materi_Sosialisasi_Lengkap.php
@@ -50,6 +50,14 @@
 
     <h1>Materi Sosialisasi Lengkap</h1>
 
+    <div class="container">
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+            <hr>
+            <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+        </div>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
